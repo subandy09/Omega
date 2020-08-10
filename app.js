4d76a48f6fd2f71c6ec99cbad4f937429858fec6
subandy09 <69459419+subandy09@users.noreply.github.com>
2020-08-10 15:58:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyAmvvM2HdNMsi_bdwsocHPLXDx_sEez67E",
+  authDomain: "rundog-fd0ba.firebaseapp.com",
+  databaseURL: "https://rundog-fd0ba.firebaseio.com",
+  projectId: "rundog-fd0ba",
+  storageBucket: "rundog-fd0ba.appspot.com",
+  messagingSenderId: "177367740510",
+  appId: "1:177367740510:web:a365a6f848ed44292204cf",
+  measurementId: "G-PEPE4MT1CR"
 };
 
      
